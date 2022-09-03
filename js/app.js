95b49e03e258d6440286672272eb4b96ef5390e4
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-09-03 20:35:01 +0000

    added spinner part

--- js/app.js
+++ js/app.js
@@ -11,12 +11,15 @@ const loadCategories = () => {
                 span.innerText = data.category_name;
                 document.getElementById('category-container').appendChild(span);
                 span.addEventListener('click', function () {
-
+                    toggleSpinner(true);
                     loadNews(data.category_id)
+                    toggleSpinner(false);
                 })
 
             })
         })
+
+
 }
 loadCategories();
 
@@ -24,8 +27,11 @@ loadCategories();
 
 const loadNews = (category_id) => {
 
+
     document.getElementById('news-container').innerHTML = ` `
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+
+
     fetch(url)
         .then(res => res.json())
         .then(data => {
@@ -38,20 +44,20 @@ const loadNews = (category_id) => {
                     }" alt="Movie"></figure>
                 <div class="card-body">
                     <h2 class="card-title">${data.title}</h2>
-                    <p>${data.details.slice(0, 150)}</p>
+                    <p>${data.details?.slice(0, 150)}...</p>
                     <div class="card-actions justify-between items-center">
                         <div class="flex justify-between">
                             <img src="${data.author.img}"
                                 class="w-12 rounded-full h-12 ring-2 ring-pink-500" alt="">
                             <div class="ml-4">
                                 <p class="font-medium">${data.author.name}</p>
-                                <p>${data.author.published_date.slice(0, 10)}</p>
+                                <p>${data.author.published_date?.slice(0, 10)}</p>
                             </div>
                         </div>
                         <div class="flex justify-between items-center space-x-2">
                             <i class="fa-regular fa-eye"></i>
                             <div>
-                                <p>${data.total_view}</p>
+                                <p class="font-bold">${data.total_view}</p>
                             </div>
                         </div>
                         <div class="text-xl">
@@ -60,24 +66,23 @@ const loadNews = (category_id) => {
                             <i class="fa-regular fa-star"></i>
                             <i class="fa-regular fa-star"></i>
                             <i class="fa-regular fa-star"></i>
-                            <i class="fa-regular fa-star"></i>
                         </div>
 <label for="modal${data._id}" class="text-violet-700 modal-button cursor-pointer text-xl"><i class="fa-solid fa-arrow-right"></i></label>
 
-<!-- Put this part before </body> tag -->
-<input type="checkbox" id="modal${data._id}" class="modal-toggle" />
-<div class="modal">
-  <div class="modal-box w-11/12 max-w-5xl p-0">
-    <figure><img src="${data.image_url}" alt="Movie"></figure>
-    <div class="p-4">
-    <h3 class="font-bold text-lg">${data.title}</h3>
-    <p class="py-4">${data.details}</p>
-    </div>
-    <div class="modal-action">
-      <label for="modal${data._id}" class="btn m-10 px-6 py-2">Yay!</label>
-    </div>
-  </div>
-</div>
+                            <!-- Put this part before </body> tag -->
+                            <input type="checkbox" id="modal${data._id}" class="modal-toggle" />
+                            <div class="modal">
+                            <div class="modal-box w-11/12 max-w-5xl p-0">
+                                <figure><img src="${data.image_url}" alt="Movie"></figure>
+                                <div class="p-4">
+                                <h3 class="font-bold text-lg">${data.title}</h3>
+                                <p class="py-4">${data.details}</p>
+                                </div>
+                                <div class="modal-action">
+                                <label for="modal${data._id}" class="btn m-10 px-6 py-2"><i class="fa-solid fa-xmark"></i></label>
+                                </div>
+                            </div>
+                            </div>
                     </div>
                 </div>
             </div>
@@ -85,5 +90,18 @@ const loadNews = (category_id) => {
                 document.getElementById('news-container').appendChild(div);
             })
         })
+
 }
 
+
+const toggleSpinner = isloading => {
+    const loaderSection = document.getElementById('loader');
+    setTimeout(() => {
+        if (isloading) {
+            loaderSection.classList.remove('hidden')
+        } else {
+            loaderSection.classList.add('hidden')
+        }
+    }, 500)
+
+}
